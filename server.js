8d5442e4f1a8889b056f73c77b6c0c48d2f6c3e4
brayan-huerfano <109161111+brayan-huerfano@users.noreply.github.com>
2024-03-26 15:03:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 // Middleware para configurar las cabeceras de CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://aurapazfunerariaysaladevelacion.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
